7300c4179a70e1966e9794711b6ff6038d602819
joey-squla <joey@squla.com>
2018-03-11 16:22:19 +0100

    Added level and player

--- app/scenes/main.js
+++ app/scenes/main.js
@@ -1,3 +1,5 @@
+import WorldManager from './worldManager'
+
 export default class Main extends Phaser.Scene{
 
     constructor(){
@@ -5,10 +7,33 @@ export default class Main extends Phaser.Scene{
     }
 
     preload(){
+        this.load.spritesheet('level_tm', 'level_tm.png',{ frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('player_sprite', 'player_tm.png',{ frameWidth: 32, frameHeight: 32 });
+        this.load.tilemapTiledJSON('map', 'map1.json');
 
     }
 
     create(){
+        this.player = this.physics.add.sprite(80, 70, 'player_sprite').setBounce(0.1);
+        this.worldManager = new WorldManager(this);
+        this.physics.add.collider(this.player, this.worldManager.groundLayer);
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
 
+    placeTiles(){
+
+    }
+
+    update(){
+        this.player.body.setVelocityX(0);
+        if(this.cursors.right.isDown) {
+            this.player.body.setVelocityX(100);
+        }
+        else if(this.cursors.left.isDown) {
+            this.player.body.setVelocityX(-100);
+        }
+        if(this.cursors.up.isDown && this.player.body.onFloor()) {
+            this.player.body.setVelocityY(-250);
+        }
     }
 }
---
+++ app/scenes/worldManager.js
@@ -0,0 +1,9 @@
+export default class WorldManager {
+
+    constructor(game){
+        this.map = game.make.tilemap({ key: 'map' });
+        let groundTiles = this.map.addTilesetImage('level_tm');
+        this.groundLayer = this.map.createDynamicLayer('Ground Layer', groundTiles, 0, 0);
+        this.groundLayer.setCollisionBetween(1, 2);
+    }
+}
